8d335bb197679c07a7b2801446d18a0aa69b18d3
Anton Adamansky <adamansky@gmail.com>
2018-04-29 13:19:34 +0700

    - minors

--- src/riot-typed.d.ts
+++ src/riot-typed.d.ts
@@ -1,13 +1,11 @@
-declare module 'riot-typed' {
-    /**
-     * decorator on class that extends Tag.
-     * that defines a riot tag with template and the class.
-     * see riot.tag()
-     */
-    export function Tag(name: string, tmpl?: string | {
-        tmpl?: string;
-        css?: string;
-        attrs?: string;
-    }): (target: Function) => void;
-    
-}
+/**
+ * decorator on class that extends Tag.
+ * that defines a riot tag with template and the class.
+ * see riot.tag()
+ */
+export function Tag(name: string, tmpl?: string | {
+    tmpl?: string;
+    css?: string;
+    attrs?: string;
+}): (target: Function) => void;
+
